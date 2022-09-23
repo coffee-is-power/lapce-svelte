a72ab8000262f3e6bf1e9ec22e4d006cb81a2f99
Tiago Dinis <tiagodinis33@gmail.com>
2022-09-23 18:52:56 +0100

    init: create project

---
+++ src/main.rs
@@ -0,0 +1,78 @@
+use anyhow::Result;
+use lapce_plugin::{
+    psp_types::{
+        lsp_types::{request::Initialize, DocumentFilter, DocumentSelector, InitializeParams, Url},
+        Request,
+    },
+    register_plugin, LapcePlugin, VoltEnvironment, PLUGIN_RPC,
+};
+use serde_json::Value;
+
+#[derive(Default)]
+struct State {}
+
+register_plugin!(State);
+
+fn initialize(params: InitializeParams) -> Result<()> {
+    let document_selector: DocumentSelector = vec![DocumentFilter {
+        // lsp language id
+        language: Some(String::from("svelte")),
+        // glob pattern
+        pattern: Some(String::from("**.svelte")),
+        // like file:
+        scheme: None,
+    }];
+    let mut server_args = vec!["--stdio".to_string()];
+    if let Some(options) = params.initialization_options.as_ref() {
+        if let Some(lsp) = options.get("lsp") {
+            if let Some(args) = lsp.get("serverArgs") {
+                if let Some(args) = args.as_array() {
+                    if !args.is_empty() {
+                        server_args = vec![];
+                    }
+                    for arg in args {
+                        if let Some(arg) = arg.as_str() {
+                            server_args.push(arg.to_string());
+                        }
+                    }
+                }
+            }
+
+            if let Some(server_path) = lsp.get("serverPath") {
+                if let Some(server_path) = server_path.as_str() {
+                    if !server_path.is_empty() {
+                        let server_uri = Url::parse(&format!("urn:{}", server_path))?;
+                        PLUGIN_RPC.start_lsp(
+                            server_uri,
+                            server_args,
+                            document_selector,
+                            params.initialization_options,
+                        );
+                        return Ok(());
+                    }
+                }
+            }
+        }
+    }
+    let server_path = Url::parse("urn:svelteserver")?;
+    PLUGIN_RPC.start_lsp(
+        server_path,
+        server_args,
+        document_selector,
+        params.initialization_options,
+    );
+    Ok(())
+}
+
+impl LapcePlugin for State {
+    fn handle_request(&mut self, _id: u64, method: String, params: Value) {
+        #[allow(clippy::single_match)]
+        match method.as_str() {
+            Initialize::METHOD => {
+                let params: InitializeParams = serde_json::from_value(params).unwrap();
+                let _ = initialize(params);
+            }
+            _ => {}
+        }
+    }
+}
